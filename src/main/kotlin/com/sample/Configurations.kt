60e0227e036a18587f0a36e754fa36cdc645f68e
cheroliv <cheroliv@laposte.net>
2020-11-19 02:45:24 +0100

    introduce spring security configuration(first step)

--- src/main/kotlin/com/sample/Configurations.kt
+++ src/main/kotlin/com/sample/Configurations.kt
@@ -7,33 +7,62 @@ import org.springframework.fu.kofu.templating.mustache
 import org.springframework.fu.kofu.webflux.webFlux
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 val dataConfig = configuration {
-	beans {
-		bean<UserRepository>()
-		bean {
-			ConnectionFactoryInitializer().apply {
-				setConnectionFactory(ref())
-				setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("db/tables.sql")))
-			}
-		}
-	}
-	r2dbc {
-		url = "r2dbc:h2:mem:///testdb;DB_CLOSE_DELAY=-1"
-	}
+    beans {
+        bean<UserRepository>()
+        bean {
+            ConnectionFactoryInitializer().apply {
+                setConnectionFactory(ref())
+                setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("db/tables.sql")))
+            }
+        }
+    }
+    r2dbc {
+        url = "r2dbc:h2:mem:///testdb;DB_CLOSE_DELAY=-1"
+    }
 }
 
 val webConfig = configuration {
-	beans {
-		bean<UserHandler>()
-		bean(::routes)
-	}
-	webFlux {
-		port = if (profiles.contains("test")) 8181 else 8080
-		mustache()
-		codecs {
-			string()
-			jackson()
-		}
-	}
+    beans {
+        bean<UserHandler>()
+        bean(::routes)
+    }
+    webFlux {
+        port = if (profiles.contains("test")) 8181 else 8080
+        mustache()
+        codecs {
+            string()
+            jackson()
+        }
+    }
+}
+
+val securityConfig = configuration {
+    org.springframework.context.support.beans {
+        bean {
+            fun user(user: String, pw: String, vararg roles: String) = User.withDefaultPasswordEncoder().username(user).password(pw).roles(*roles).build()
+            InMemoryUserDetailsManager(user("jlong", "pw", "USER"), user("rwinch", "pw1", "USER", "ADMIN"))
+        }
+    }
+}
+
+@EnableWebSecurity
+open class KotlinSecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http {
+            httpBasic {}
+            authorizeRequests {
+                authorize("/greetings/**", hasAuthority("ROLE_ADMIN"))
+                authorize("/**", permitAll)
+            }
+        }
+    }
 }
